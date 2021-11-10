398b17f1fbecbfb65fcea4e216d49e1d0ec22214
ryooookada <btbl70120@gmail.com>
2021-11-10 07:07:37 +0000

    [Add]practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :authenticate_user!, except: [:top, :about]
+  
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+  
+  def after_update_path_for(resource)
+    users_path(resource)
+  end
+  
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end 
+  
   protected
 
   def configure_permitted_parameters
